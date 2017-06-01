d9c7d534123f67848fb2e9529191ea32a3b640b0
Aleksandar Jankovic <aleksonline@icloud.com>
2017-06-01 22:27:34 +0200

    Alien-inspired stuffs

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,6 +1,53 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var basePosition = 800;
+var paragraphs = document.getElementsByTagName('P');
+
+for (var i = 0; i < paragraphs.length; i += 1) {
+  var paragraph = paragraphs[i];
+  paragraph.firstElementChild.classList.add('hide');
+  paragraph.setAttribute('id', 'paragraph_' + i);
+}
+
+var lines = Array.from(paragraphs).map(function (paragraph) {
+  var draw = SVG(paragraph.getAttribute('id')).size(basePosition, '1.7em');
+  var line = draw.rect('1em', '.4em').attr({ x: basePosition, y: '.7em', fill: '#d2fed3' });
+
+  return { drawArea: draw, paragraph: paragraph, line: line, text: paragraph.firstElementChild.innerText };
+});
+
+setTimeout(function () {
+  animate(lines);
+}, 1000);
+
+function animate(lines) {
+  if (lines && lines.length > 0) {
+    var lineObj = lines.shift();
+    var lineAnimationSpeed = lineObj.paragraph.firstElementChild.clientWidth * 2;
+    var lineLength = lineObj.paragraph.firstElementChild.clientWidth;
+
+    lineObj.line.animate({ duration: 150, ease: '-' }).width(basePosition).move(0, '.7em').queue(function () {
+      var marker = lineObj.drawArea.rect('.2em', '1.7em').attr({ x: 0, y: 0, fill: '#d2fed3' });
+
+      marker.animate({ duration: lineAnimationSpeed, ease: '-' }).move(lineLength, 0).once(1, function () {
+        marker.addClass('hide');
+      });
+
+      lineObj.line.attr({ y: 0 }).height('1.7em');
+      lineObj.line.style({ fill: '#000000' });
+      lineObj.paragraph.firstElementChild.classList.remove('hide');
+      lineObj.line.fx.dequeue();
+    }).animate({ duration: lineAnimationSpeed + 20, ease: '-' }).size(0, '1.5em').move(lineLength, 0).delay(300).queue(function () {
+      lineObj.line.fx.dequeue();
+      animate(lines);
+    });
+  }
+}
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
 var contentElements = document.getElementById('main').children;
 var latestContentElement = contentElements[0];
 var collapsedElement = void 0;
@@ -160,4 +207,183 @@ function start() {
 
 start();
 
-},{}]},{},[1]);
+},{}],3:[function(require,module,exports){
+module.exports={
+  "cards": [
+    {
+      "name": "Tear Gas",
+      "type": "Action",
+      "subType": "Action",
+      "description": [
+        "Discard cards from the top of your Inventory until you discard a Weapon.",
+        "If you Discarded a Weapon, your character, your character deals an additional 10 Damage during this turn for each card you Discarded."
+      ],
+      "unique": false,
+      "cost": 70,
+      "trashAfterUse": false,
+      "ammoRequirement": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "damage": {
+        "minimum": 30,
+        "isAdjustable": true
+      },
+      "extra": {
+        "actions": {
+          "amount": 1,
+          "guaranteed": true
+        },
+        "buys": {
+          "amount": 0,
+          "guaranteed": false
+        },
+        "explores": {
+          "amount": 0,
+          "guaranteed": false
+        }
+      },
+      "tags": ["weaponFinder"]
+    }, {
+      "name": "Msater of Unlocking",
+      "type": "Action",
+      "subType": "Action",
+      "description": [
+        "Each other Player Reveals the top card of their Inventory.",
+        "You can gain 1 of the Revealed Weapons.",
+        "All of the other Revealed cards are discarded afterwards."
+      ],
+      "unique": false,
+      "cost": 30,
+      "trashAfterUse": false,
+      "ammoRequirement": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "damage": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "extra": {
+        "actions": {
+          "amount": 1,
+          "guaranteed": true
+        },
+        "buys": {
+          "amount": 0,
+          "guaranteed": false
+        },
+        "explores": {
+          "amount": 0,
+          "guaranteed": false
+        }
+      },
+      "tags": ["pvp", "cardStealer"]
+    }, {
+      "name": "Uroboros Injection",
+      "type": "Action",
+      "subType": "Action",
+      "description": [
+        "During another Player's turn, when an Infected is Revealed, you can Trash this card from your hand to give it +20 Health to any Revealed Infected during this turn."
+      ],
+      "unique": false,
+      "cost": 60,
+      "trashAfterUse": false,
+      "ammoRequirement": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "damage": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "extra": {
+        "actions": {
+          "amount": 2,
+          "guaranteed": true
+        },
+        "buys": {
+          "amount": 1,
+          "guaranteed": true
+        },
+        "explores": {
+          "amount": 0,
+          "guaranteed": false
+        }
+      },
+      "tags": ["pvp", "enemyBooster"]
+    }, {
+      "name": "The Gathering Darkness",
+      "type": "Action",
+      "subType": "Action",
+      "description": [
+        "Trash this card and choose another Player.",
+        "That Player Reveals their Hand and Trashes an Ammunition with the higehst cost from their Hand.",
+        "That Player moves 1 \"Ammo x10\" Ammunition to their hand for every 10 ammo the Trashed card provided."
+      ],
+      "unique": false,
+      "cost": 50,
+      "trashAfterUse": true,
+      "ammoRequirement": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "damage": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "extra": {
+        "actions": {
+          "amount": 0,
+          "guaranteed": false
+        },
+        "buys": {
+          "amount": 0,
+          "guaranteed": false
+        },
+        "explores": {
+          "amount": 0,
+          "guaranteed": false
+        }
+      },
+      "tags": ["pvp", "deckDiluter"]
+    }, {
+      "name": "The Gathering Darkness",
+      "type": "Action",
+      "subType": "Action",
+      "description": [
+        "Choose another Player.",
+        "That Player Reveales their hand and you select and Discard a Weapon from it (if any).",
+        "Your Character deals and additional X Damage during this turn, where X = the damage of the Discarded Weapon."
+      ],
+      "unique": false,
+      "cost": 70,
+      "trashAfterUse": false,
+      "ammoRequirement": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "damage": {
+        "minimum": 0,
+        "isAdjustable": false
+      },
+      "extra": {
+        "actions": {
+          "amount": 0,
+          "guaranteed": false
+        },
+        "buys": {
+          "amount": 0,
+          "guaranteed": false
+        },
+        "explores": {
+          "amount": 0,
+          "guaranteed": false
+        }
+      },
+      "tags": ["pvp", "weaponBooster"]
+    }
+  ]
+}
+
+},{}]},{},[1,2,3]);
---
+++ source/scripts/alien.js
@@ -0,0 +1,54 @@
+const basePosition = 800;
+const paragraphs = document.getElementsByTagName('P');
+
+for (let i = 0; i < paragraphs.length; i+= 1) {
+  const paragraph = paragraphs[i];
+  paragraph.firstElementChild.classList.add('hide');
+  paragraph.setAttribute('id', `paragraph_${i}`);
+}
+
+const lines = Array.from(paragraphs).map((paragraph) => {
+  const draw = SVG(paragraph.getAttribute('id')).size(basePosition, '1.7em');
+  const line = draw.rect('1em', '.4em').attr({ x: basePosition, y: '.7em', fill: '#d2fed3' });
+
+  return { drawArea: draw, paragraph, line, text: paragraph.firstElementChild.innerText };
+});
+
+setTimeout(() => { animate(lines); }, 1000);
+
+function animate(lines) {
+  if (lines && lines.length > 0) {
+    const lineObj = lines.shift();
+    const lineAnimationSpeed = lineObj.paragraph.firstElementChild.clientWidth * 2;
+    const lineLength = lineObj.paragraph.firstElementChild.clientWidth;
+
+    lineObj.line
+      .animate({ duration: 150, ease: '-' })
+      .width(basePosition)
+      .move(0, '.7em')
+      .queue(() => {
+        const marker = lineObj
+          .drawArea
+          .rect('.2em', '1.7em')
+          .attr({ x: 0, y: 0, fill: '#d2fed3' });
+
+        marker.animate({ duration: lineAnimationSpeed, ease: '-' })
+          .move(lineLength, 0)
+          .once(1, () => { marker.addClass('hide'); });
+
+        lineObj.line.attr({ y: 0 }).height('1.7em');
+        lineObj.line.style({ fill: '#000000' });
+        lineObj.paragraph.firstElementChild.classList.remove('hide');
+        lineObj.line.fx.dequeue();
+      })
+      .animate({ duration: lineAnimationSpeed + 20, ease: '-' })
+      .size(0, '1.5em')
+      .move(lineLength, 0)
+      .delay(300)
+      .queue(() => {
+        lineObj.line.fx.dequeue();
+        animate(lines);
+      }
+    );
+  }
+}
