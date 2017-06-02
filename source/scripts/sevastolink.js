c2d180ef3a087124c48ce094783120f526b4a7f9
Aleksandar Jankovic <aleksonline@icloud.com>
2017-06-02 16:14:15 +0200

    Improved loading animation. Less calls

--- source/scripts/sevastolink.js
+++ source/scripts/sevastolink.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const baseHeight = 800;
-const baseWidth = 1024;
 const loading = document.getElementById('loading');
 const login = document.getElementById('login');
 const loggedInLoading = document.getElementById('loggedInLoading');
@@ -10,10 +8,13 @@ const loadingBar = document.getElementById('loadingBar');
 function createLoadingScreen(callback) {
   const drawArea = SVG(document.createDocumentFragment());
   const rects = [];
+  const hideRects = [];
 
-  for (let i = 0; i < 22; i += 1) {
-    for (let j = 0; j < 16; j += 1) {
-      rects.push(drawArea.rect('1.5em', '1.5em').attr({ x: i * 3 + 'em', y: j * 3 + 'em', fill: '#02c45a' }));
+  for (let row = 0; row < 21; row += 1) {
+    hideRects.push(drawArea.rect('64em', 0).attr({ x: 0, y: `${row * 3}em`, fill: '#011d00' }));
+
+    for (let column = 0; column < 16; column += 1) {
+      rects.push(drawArea.rect('1.5em', '1.5em').attr({ x: `${row * 3}em`, y: `${column * 3}em`, fill: '#02c45a' }));
     }
   }
 
@@ -21,8 +22,9 @@ function createLoadingScreen(callback) {
     loading.innerHTML = '';
     loading.classList.remove('hide');
     loading.appendChild(drawArea.node);
-    rects.forEach((rect) => {
-      return rect.animate({ duration: 30 }).size('1.5em', '1em').delay(70).animate({ duration: 30 }).size('1.5em', '.5em').delay(70).animate({ duration: 30 }).size('1.5em', 0);
+    hideRects.forEach((rect) => {
+      rect.front();
+      return rect.animate({ duration: 30 }).size('64em', '.5em').delay(70).animate({ duration: 30 }).size('64em', '1em').delay(70).animate({ duration: 30 }).size('64em', '1.5em');
     });
 
     setTimeout(() => {
@@ -44,7 +46,14 @@ setTimeout(() => {
       .animate({ duration: 500 })
       .size('6em', '.8em')
       .delay(500)
-      .animate({ duration: 200 }).size('12em', '.8em').delay(300).animate({ duration: 700 }).size('15em', '.8em').delay(800).animate({ duration: 50 }).size('16em', '.8em');
+      .animate({ duration: 200 })
+      .size('12em', '.8em')
+      .delay(300)
+      .animate({ duration: 700 })
+      .size('15em', '.8em')
+      .delay(800)
+      .animate({ duration: 50 })
+      .size('16em', '.8em');
 
     loggedInLoading.classList.remove('hide');
     loadingBar.appendChild(drawArea.node);
