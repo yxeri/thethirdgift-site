e761b4e153f42e8a8f2aec2ad7595a42ffa02d55
Aleksandar Jankovic <aleksonline@icloud.com>
2017-06-02 12:01:32 +0200

    Sevastolink boot sequence

--- source/scripts/sevastolink.js
+++ source/scripts/sevastolink.js
@@ -5,6 +5,7 @@ const baseWidth = 1024;
 const loading = document.getElementById('loading');
 const login = document.getElementById('login');
 const loggedInLoading = document.getElementById('loggedInLoading');
+const loadingBar = document.getElementById('loadingBar');
 
 function createLoadingScreen(callback) {
   const drawArea = SVG(document.createDocumentFragment());
@@ -17,6 +18,7 @@ function createLoadingScreen(callback) {
   }
 
   setTimeout(() => {
+    loading.innerHTML = '';
     loading.classList.remove('hide');
     loading.appendChild(drawArea.node);
     rects.forEach((rect) => {
@@ -33,6 +35,25 @@ function createLoadingScreen(callback) {
 setTimeout(() => {
   login.classList.add('hide');
   createLoadingScreen(() => {
+    const drawArea = SVG(document.createDocumentFragment()).size('16em', '.8em');
+    const rect = drawArea.rect('16em', '.8em').attr({ fill: '#011d00', stroke: '#02c45a' });
+    rect.addClass('outerBar');
+    drawArea
+      .rect(0, '.8em')
+      .attr({ fill: '#02c45a' })
+      .animate({ duration: 500 })
+      .size('6em', '.8em')
+      .delay(500)
+      .animate({ duration: 200 }).size('12em', '.8em').delay(300).animate({ duration: 700 }).size('15em', '.8em').delay(800).animate({ duration: 50 }).size('16em', '.8em');
 
+    loggedInLoading.classList.remove('hide');
+    loadingBar.appendChild(drawArea.node);
+
+    setTimeout(() => {
+      loggedInLoading.classList.add('hide');
+      createLoadingScreen(() => {
+
+      });
+    }, 3200)
   });
 }, 500);
